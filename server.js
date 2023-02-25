3770b4d2ff35218c4e206c8ef81b0c4b49372ba6
dddingnan <silenceapple1027@gmail.com>
2023-02-25 01:08:51 -0500

    config: update cors configuration.

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081",
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
 };
-
 app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
