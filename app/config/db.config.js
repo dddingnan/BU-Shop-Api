6f19ee865b60ffba4a4d3241d8465829fed86214
dddingnan <silenceapple1027@gmail.com>
2023-03-13 23:29:22 -0400

    config: update config.

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  HOST: "bu-course.mysql.database.azure.com",
-  USER: "dingnan",
-  PASSWORD: "@ddtest123",
-  DB: "cs602",
+  HOST: "aaa",
+  USER: "ssss",
+  PASSWORD: "@sadad",
+  DB: "asdasd",
 };
